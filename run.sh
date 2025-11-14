5057d8907b1a1c866ed03263b636665de5dd18a5
skylineagle <skylineagle@proton.me>
2025-11-14 15:25:21 +0200

    feat: running util script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker run --rm -it \
+	-v $PWD/gtfs-tmp:/tmp/gtfs \
+	-v $PWD/preprocess.sh:/etc/gtfs/preprocess.sh \
+	-v $PWD/custom-download.sh:/importer/custom-download.sh \
+	-e 'GTFS_DOWNLOAD_SCRIPT=/importer/custom-download.sh' \
+	-e 'GTFS_DOWNLOAD_USER_AGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' \
+	-e 'GTFS_IMPORTER_SCHEMA=public' \
+	-e 'GTFS_IMPORTER_DB_PREFIX=gtfs' \
+	-e 'GTFS_DOWNLOAD_URL=https://gtfs.mot.gov.il/gtfsfiles/israel-public-transportation.zip' \
+	-e 'PGHOST=aws-1-ap-south-1.pooler.supabase.com' \
+	-e 'PGPORT=6543' \
+	-e 'PGDATABASE=postgres' \
+	-e 'PGUSER=postgres.oxelinxseckxpdjpkpzf' \
+	-e 'PGPASSWORD=autoto-sandbox' \
+	ghcr.io/mobidata-bw/postgis-gtfs-importer:v5
